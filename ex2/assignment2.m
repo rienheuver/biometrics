9de8374a06efb6f953d986f5fe53d066b313bc74
Rien Heuver <rienheuver@gmail.com>
2018-12-18 16:31:57 +0100

    Not working, splitting matrix

--- ex2/assignment2.m
+++ ex2/assignment2.m
@@ -1,2 +1,9 @@
 load('FaceData.mat');
-imshow(FaceData(39,9).Image);
+faceTrain = struct([]);
+faceTest = struct;
+for i=1:20
+    for j=1:10
+        imageDouble = double(FaceData(i,j).Image)/255;
+        faceTrain.i.j = imageDouble(:);
+    end
+end
