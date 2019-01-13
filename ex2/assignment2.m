aa718f9839227bc0e18f97cbe9ae1e186d02c45f
André Pinto <andrepinto@Andres-MacBook-Pro.local>
2019-01-13 20:58:20 +0100

    Done the eigenvalues

--- ex2/assignment2.m
+++ ex2/assignment2.m
@@ -4,8 +4,8 @@ q = 46; % image height
 d = p*q;
 subjectCount = 40;
 facesPerSubject = 10;
-n = (subjectCount/2) * facesPerSubject;
-l = (subjectCount/2) * facesPerSubject;
+n = (subjectCount/2) * facesPerSubject; %number of samples for training
+l = (subjectCount/2) * facesPerSubject; %number of samples for testing
 faceTrain = zeros(d, n); % n*d matrix
 faceTest = zeros(d, l); % l%d matrix
 Id = zeros(1, l); % l%1 matrix
@@ -26,13 +26,24 @@ for i=(subjectCount/2 + 1):subjectCount
     end
 end
 %%
-phi0 = mean(faceTrain,2);
+phi0 = mean(faceTrain,2); %column vector with the mean value for each pixel
 faceTrain0 = zeros(d, n);
 for i=1:n
-    faceTrain0(:, i) = faceTrain(:, i) - phi0;
+    faceTrain0(:, i) = faceTrain(:, i) - phi0; % subtracting to each image the mean vector
 end
 
-
+order_eigen_value = zeros(p*q,1);
 covMatrix = 1/(n-1) * faceTrain0 * faceTrain0';
-
-%test
+[eig_vec,eig_value] = eig(covMatrix);
+diag_eig_value = diag(eig_value);
+sum_eig_value = sum(diag_eig_value);
+%% Compute the information present in m principal components
+v = zeros(d,1);
+sum_currently = 0;
+m = (1:d)';
+for i = 1:d
+    sum_currently = sum_currently + diag_eig_value(i);
+    v(i) = sum_currently/sum_eig_value;
+end
+figure();
+plot(m,v)
