8db1ddfbd11db8d9fd1935b7f3ed2c4cba526428
Rien Heuver <rienheuver@gmail.com>
2019-01-22 16:56:49 +0100

    Finished assignment 2

--- ex2/assignment2.m
+++ ex2/assignment2.m
@@ -10,17 +10,17 @@ faceTrain = zeros(d, n); % n*d matrix
 faceTest = zeros(d, l); % l%d matrix
 Id = zeros(1, l); % l%1 matrix
 %%
-for i=1:subjectCount/2
-    for j=1:facesPerSubject
+for i=1:subjectCount/2 % 1 -> 20
+    for j=1:facesPerSubject % 1 -> 10
         imageDouble = double(FaceData(i,j).Image)/255;
-        faceTrain(:, i*j) = imageDouble(:);
+        faceTrain(:, (i-1)*10+j) = imageDouble(:);
     end
 end
 
-for i=(subjectCount/2 + 1):subjectCount
-    for j=1:facesPerSubject
+for i=(subjectCount/2 + 1):subjectCount % 21 -> 40
+    for j=1:facesPerSubject % 1 -> 10
         imageDouble = double(FaceData(i,j).Image)/255;
-        faceTest(:, (i-20)*j) = imageDouble(:);
+        faceTest(:, (i-21)*10+j) = imageDouble(:);
         
         % Calculates row number in Id-matrix (1 - 200) from for-loops
         Id(:, (i-21)*10 + j) = i;
@@ -29,7 +29,7 @@ end
 %%
 for i=1:5
     for j=1:10
-        figure, imshow(FaceData(i,j).Image);
+        %figure, imshow(FaceData(i,j).Image);
     end
 end
 %%
@@ -58,25 +58,125 @@ plot(m,v)
 % Show mean face
 figure, imagesc(reshape(phi0, [56,46]));
 
-m = 90;
-phi_m = eig_vec(:,1:m);
-% Show first 10 faces
-for i=1:10
-    faceColumn = phi_m(:,i);
-    figure, imagesc(reshape(faceColumn,[56,46]));
-end
-%% Calculate scores
-dissimilarityScores = zeros(l,l);
-matrixA = zeros(m,l);
-for i=1:l
-    matrixA(:, i) = phi_m'*(faceTest(:,i)-phi0);
-end
-
-disSimScore = zeros(l,l);
-for i=1:l
-    for j=1:l
-        disSimScore(i,j) = norm(matrixA(:,i) - matrixA(:, j));
+for m=10:10:100
+    phi_m = eig_vec(:,1:m);
+    % Show first 10 faces
+    for i=1:10
+        faceColumn = phi_m(:,i);
+        %figure, imagesc(reshape(faceColumn,[56,46]));
     end
+    %% Calculate scores
+    dissimilarityScores = zeros(l,l);
+    matrixA = zeros(m,l);
+    for i=1:l
+        matrixA(:, i) = phi_m'*(faceTest(:,i)-phi0);
+    end
+
+    disSimScore = zeros(l,l);
+    for i=1:l
+        for j=1:l
+            disSimScore(i,j) = norm(matrixA(:,i) - matrixA(:, j));
+        end
+    end
+    %%
+     %%Loads score matrix and plots info.
+    S = disSimScore;
+    [np, nt] = size(S);
+    nId = 20;
+    Entries = (1:np);
+    Ratio = np / nId;
+    %% Creating a dictionary that links each individual to each of his scores
+    fprintf(' Size of score matrix: %u x %u\n',np,nt);
+    fprintf(' Number of identities: %u\n', nId);
+    fprintf(' Ratio: %u\n', Ratio);
+
+    % figure(1); plot(Entries, Id); 
+    % xlabel('Entry'); ylabel('Identity'); title('Mapping entry number to identity');
+    %  
+    % figure(2); imagesc(S); colormap('gray');
+    % ylabel('test'); xlabel('reference'); title('Score matrix');
+
+    idCountMap = containers.Map('KeyType', 'single', 'ValueType', 'single');
+    currentId = 21;
+    currentCount = 0;
+    for i = Id
+        if i == currentId
+            currentCount = currentCount + 1;
+        else
+            idCountMap(currentId) = currentCount;
+            currentId = currentId + 1;
+            currentCount = 1;
+        end
+    end
+    idCountMap(currentId) = currentCount;
+    %%
+    genuineScores = zeros(1, 900);
+    genuineCounter = 0;
+    for i=1:20 % person 2
+        for j=1:10 % 
+            for k=1:10 % 
+                if k > j
+                    genuineCounter = genuineCounter + 1;
+                    genuineScores(genuineCounter) = S(j+(i-1)*10,k+(i-1)*10);
+                end
+            end
+        end
+    end
+    disp("Finished making genuine scores");
+    %%
+    imposterScores = zeros(1,19000);
+    imposterCounter = 0;
+    % Loop over persons
+    for i=1:20 % 1 -> 20
+        % Loop from right part of yellow block until right side of matrix
+        for j=i*10+1:length(S) % 11 -> 200
+            % Loop from left/top part of yellow block until right/bottom of
+            % yellow block (amount of rows in a yellow block)
+            for k=(i-1)*10+1:i*10
+                % Add scores to array
+                imposterCounter = imposterCounter + 1;
+                imposterScores(imposterCounter) = S(j,k);
+            end
+        end
+    end
+    disp("Finished making imposter scores");
+    %% Plotting the histograms
+    disp("Plotting histograms");
+    numberOfBins = 300;
+    %figure();
+    %genuineHist = histogram(genuineScores, numberOfBins);
+    %hold on
+    %imposterHist = histogram(imposterScores, numberOfBins);
+    %hold off
+    %disp("Finished plotting histograms");
+    %% plotting the perfomance characteristics based on a threshold
+    FMR = [];
+    FNMR = [];
+    DET = [];
+    bestMatch = 1;
+    bestThreshold = 0;
+    EER = 0;
+    for i=0:.001:10
+        FMR_current = sum(imposterScores < i)/length(imposterScores);
+        FNMR_current = 1 - (sum(genuineScores < i)/length(genuineScores));
+        FMR = [FMR,[i;FMR_current]];
+        FNMR = [FNMR,[i; FNMR_current]];
+        EER_current = abs(FMR_current - FNMR_current);
+        if EER_current < bestMatch
+            bestMatch = EER_current;
+            EER = FMR_current;
+            bestThreshold = i;
+        end
+        DET = [DET; [FMR_current,FNMR_current]];
+    end
+    disp("Best threshold: ");
+    disp(bestThreshold);
+    disp("EER: ");
+    disp(EER);
+    %plot the ROC
+    figure;
+    plot(DET(:,1),1-DET(:,2));
+    title('ROC');
+    xlabel('FMR');
+    ylabel('TMR');
 end
-%%
-get_scores_from_file(disSimScore, Id);
